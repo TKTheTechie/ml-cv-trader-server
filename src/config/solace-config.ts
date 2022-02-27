c2314206e5f3624982f88adf405f4f27b3ccd6fd
TKTheTechie <thomas.kunnumpurath@solace.com>
2022-02-27 01:26:40 -0500

    fixes

--- src/config/solace-config.ts
+++ src/config/solace-config.ts
@@ -1,6 +1,6 @@
 export const solaceConfig = {
   solace_hostUrl: process.env.solace_hostUrl || "wss://mr8gl75znrycq.messaging.solace.cloud:443",
   solace_vpn: process.env.solace_vpn || "ml-cv-trader",
-  solace_userName: process.env.solace_userName || "b1mqkeh2lf8bc7d2uf9ks2a6kd",
-  solace_password: process.env.solace_password || "solace-cloud-client"
+  solace_userName: process.env.solace_userName || "solace-cloud-client",
+  solace_password: process.env.solace_password || "b1mqkeh2lf8bc7d2uf9ks2a6kd"
 };
